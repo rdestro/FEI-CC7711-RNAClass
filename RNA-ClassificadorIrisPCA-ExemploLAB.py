1c26b427451c6e2f05dcfd74b9bfe629b3382313
R. Destro <destro@fei.edu.br>
2025-03-25 17:38:41 -0300

    Update RNA-ClassificadorIrisPCA-ExemploLAB.py

--- RNA-ClassificadorIrisPCA-ExemploLAB.py
+++ RNA-ClassificadorIrisPCA-ExemploLAB.py
@@ -1,7 +1,8 @@
+
 from sklearn.datasets import load_iris
 
 from sklearn.neural_network import MLPClassifier
-from sklearn.metrics import plot_confusion_matrix
+from sklearn.metrics import confusion_matrix,ConfusionMatrixDisplay
 from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
 
@@ -11,19 +12,22 @@ data = load_iris()
 features =data.data
 target = data.target
 
+neuronios = (10,10,10)
+epocas = 5000
 
 plt.figure(figsize=(16,8))
 plt.subplot(2,2,1)
+plt.title("Feature originais ()")
 plt.scatter(features[:,0], features[:,1], c=target,marker='o',cmap='viridis')
 
-Classificador = MLPClassifier(hidden_layer_sizes = (10), alpha=1, max_iter=100)
+Classificador = MLPClassifier(hidden_layer_sizes = neuronios, alpha=1, max_iter=epocas, n_iter_no_change=epocas)
 Classificador.fit(features,target)
 predicao = Classificador.predict(features)
 
 plt.subplot(2,2,3)
+plt.title("Classificação sem PCA")
 plt.scatter(features[:,0], features[:,1], c=predicao,marker='d',cmap='viridis',s=150)
 plt.scatter(features[:,0], features[:,1], c=target,marker='o',cmap='viridis',s=15)
-
 
 
 
@@ -34,23 +38,32 @@ pca_features = pca.transform(features)
 print('Mantida %5.2f%% da informação do conjunto inicial de dados'%(sum(pca.explained_variance_ratio_)*100))
 
 plt.subplot(2,2,2)
+plt.title("Feature com PC (2 componentes)")
 plt.scatter(pca_features[:,0], pca_features[:,1], c=target,marker='o',cmap='viridis')
 
 
-ClassificadorPCA = MLPClassifier(hidden_layer_sizes = (10), alpha=1, max_iter=1000)
+ClassificadorPCA = MLPClassifier(hidden_layer_sizes = neuronios, alpha=1, max_iter=epocas, n_iter_no_change=epocas)
 ClassificadorPCA.fit(pca_features,target)
 
 
-predicao = ClassificadorPCA.predict(pca_features)
+pca_predicao = ClassificadorPCA.predict(pca_features)
 
 plt.subplot(2,2,4)
-plt.scatter(pca_features[:,0], pca_features[:,1], c=predicao,marker='d',cmap='viridis',s=150)
+plt.title("Classificação com PCA")
+plt.scatter(pca_features[:,0], pca_features[:,1], c=pca_predicao,marker='d',cmap='viridis',s=150)
 plt.scatter(pca_features[:,0], pca_features[:,1], c=target,marker='o',cmap='viridis',s=15)
+#plt.show()
+
+
+cm = confusion_matrix(target, predicao)
+disp = ConfusionMatrixDisplay(confusion_matrix=cm,display_labels=data.target_names)
+disp.plot()
 plt.show()
 
 
-plot_confusion_matrix(Classificador, features, target,include_values=True,display_labels=data.target_names)
+cm_pca = confusion_matrix(target, pca_predicao)
+disp = ConfusionMatrixDisplay(confusion_matrix=cm_pca,display_labels=data.target_names)
+disp.plot()
 plt.show()
 
-plot_confusion_matrix(ClassificadorPCA, pca_features, target,include_values=True,display_labels=data.target_names)
-plt.show()
+
